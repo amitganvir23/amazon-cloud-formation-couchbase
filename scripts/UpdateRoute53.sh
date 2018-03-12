6454adada9cfee0340ccf66c09a2fa312e47665f
Open Source book <amitganvir6@gmail.com>
2018-03-12 12:16:05 +0530

    UpdateRoute53.sh
    To update Route 53 entries dynamically.

---
+++ scripts/UpdateRoute53.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+echo "Running UpdateRoute53.sh"
+
+stackName=$1
+
+
+yum install ansible --enablerepo=epel -y
+echo stackName \'$stackName\'
+
+
+if [ $( grep -cw ansible_connection /etc/ansible/hosts) -eq "0" ];then
+cat >>/etc/ansible/hosts <<EOF
+[localhost]
+localhost ansible_connection=local ansible_python_interpreter=python
+EOF
+fi
+
+region=ap-south-1
+zone_name=glp-test.com
+rec_name=test.glp-test.com
+ec2_tag_key=Name
+ec2_tag_value=Couchbase-${stackName}-Server
+
+cat > route53.yml <<EOF
+---
+- hosts: localhost
+ vars:
+       - REGION: ${region}
+       - zone_name: ${zone_name}
+       - rec_name: ${rec_name}
+       - ec2_tag_key: ${ec2_tag_key}
+       - ec2_tag_value: ${ec2_tag_value}
+
+ tasks:
+  - ec2_remote_facts:
+     region: "{{REGION}}"
+     filters:
+      instance-state-name: running
+      "tag:Name": {{ec2_tag_value}}
+    register: ec2_remote_facts
+
+  - set_fact: public_ip="{{public_ip|default([])+[item.public_ip_address]}}"
+    with_items: "{{ ec2_remote_facts.instances }}"
+
+  - name: Updatading route 53
+    route53:
+     state: present
+     overwrite: true
+     private_zone: true
+     zone: "{{zone_name}}"
+     record: "{{rec_name}}"
+     type: A
+     ttl: 30
+     value: "{{public_ip}}"
+EOF
+ansible-playbook route53.yml
