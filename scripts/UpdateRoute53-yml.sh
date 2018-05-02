7b8c3523a3efaded2dd2e4faff06914123d49529
Ganvir, Amit <amit.ganvir@synapse.com>
2018-05-02 14:49:18 +0530

    Update route53df

--- scripts/UpdateRoute53-yml.sh
+++ scripts/UpdateRoute53-yml.sh
@@ -2,6 +2,7 @@
 
 echo "Running UpdateRoute53.sh"
 stackName=$1
+#stackName=glp-test
 
 yum install epel-release -y
 curl -O https://bootstrap.pypa.io/get-pip.py
@@ -14,28 +15,42 @@ yum install ansible --enablerepo=epel -y
 
 echo stackName \'$stackName\'
 
-region=us-west-1
-zone_name=glp-test.com
-rec_name=test.glp-test.com
+#ec2_tag_key=Couchbase-Cluster
+#ec2_tag_value=${stackName}
+
+
+## AWS region and credentails
+region=$2
+#region=us-west-1
+
+## Route53 zone name and record name
+zone_name=$3
+rec_name=$4
+#zone_name=glp-test.com
+#rec_name=test.glp-test.com
+
+## Ec2-tag
+ec2_tag_key=$5
 #ec2_tag_key=Name
-#ec2_tag_value=Couchbase-${stackName}-ServerRally
-#ec2_tag_value=Couchbase-${stackName}-Server
+ec2_tag_value=$6
+###-Server and ServerRally covered in value
+#ec2_tag_value=Couchbase-${stackName}-Server*
 
-ec2_tag_key=Couchbase-Cluster
-ec2_tag_value=${stackName}
 
+## Making Inventory file
 my_inventory=myhosts
-
-cat > $my_inventory <<EOF
+cat > /${my_inventory} <<EOF
 [localhost]
 localhost ansible_connection=local ansible_python_interpreter=python
 EOF
 
-echo "-----------------------"
-echo -e "zone_name=$zone_name \nRegion=$region \nRecorName=$rec_name \nec2_tag_key=$ec2_tag_key \nec2_tag_value=$ec2_tag_value\nzone_id=$zone_id \nstackName=$stackName"
-echo "-----------------------"
+## Printing Variable values
+echo "==========================="
+echo -e "zone_name=$zone_name \nRegion=$region \nRecorName=$rec_name \nec2_tag_key=$ec2_tag_key \nec2_tag_value=$ec2_tag_value"
+echo "==========================="
 
-cat > route53.yml <<EOF
+## Creating Yaml file
+cat > /route53.yml <<EOF
 ---
 - hosts: localhost
   vars:
@@ -46,10 +61,13 @@ cat > route53.yml <<EOF
        - ec2_tag_value: ${ec2_tag_value}
 
   tasks:
+   - name: sleep for 4 seconds and continue with play
+     wait_for: timeout=4
+
    - name: Collecting Private IP address
      shell: "aws --region {{REGION}} ec2 describe-instances --filters \"Name=tag:{{ec2_tag_key}},Values={{ec2_tag_value}}\" \"Name=network-interface.addresses.private-ip-address,Values=*\" --query 'Reservations[*].Instances[*].{InstanceId:InstanceId,PrivateDnsName:PrivateDnsName,State:State.Name, IP:NetworkInterfaces[0].PrivateIpAddress}'|grep -w IP|awk '{print \$2}'|tr -d ','|tr -d '\"'"
      register: private_ips
-     
+
    - set_fact: private_ip="{{private_ip|default([])+[item]}}"
      with_items: "{{ private_ips.stdout_lines }}"
 
@@ -62,7 +80,16 @@ cat > route53.yml <<EOF
       record: "{{rec_name}}"
       type: A
       ttl: 30
-      value: "{{private_ip}}"
+      value: "{{private_ips.stdout_lines}}"
+      #value: "{{private_ip}}"
+
+   - cron:
+       name: "a job for reboot"
+       special_time: reboot
+       job: "ansible-playbook -i /${my_inventory} /route53.yml -vv > /route53-cron.log"
+
 
 EOF
-ansible-playbook -i $my_inventory route53.yml -vv
+## Playing playbook to update Route53
+ansible-playbook -i /${my_inventory} /route53.yml -vv
+
